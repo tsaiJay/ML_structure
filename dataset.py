071640e83a1857647c78e9db3dabdd618eedb35a
tsaiJay <121831288+tsaiJay@users.noreply.github.com>
2024-06-25 23:08:14 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -4,8 +4,8 @@ import torchvision
 def datasetSelector(args, train_transform, test_transfrom):
     
     if args.dataset == 'cifar10':
-        train_dataset = torchvision.datasets.CIFAR10(root=args.data_path, train=True, download=False, transform=train_transform)
-        test_dataset = torchvision.datasets.CIFAR10(root=args.data_path, train=False, download=False, transform=test_transfrom)
+        train_dataset = torchvision.datasets.CIFAR10(root=args.data_path, train=True, download=True, transform=train_transform)
+        test_dataset = torchvision.datasets.CIFAR10(root=args.data_path, train=False, download=True, transform=test_transfrom)
 
     return train_dataset, test_dataset
 
